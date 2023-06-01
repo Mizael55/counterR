3b4fc900c3d7d9a93deec2f74b0e7f5e801b94bb
Mizael55 <misaellsoler@gmail.com>
2023-06-01 13:14:19 -0400

    adding funtionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 
 import './App.css';
-import freeCodeCampLogo from './img/freecodecamp-logo.png'
+import React, { useState } from 'react';
+import Boton from './components/boton';
+import Contador from './components/contador';
 
 function App() {
+  const [numClics, setNumClic] = useState(0);
+
+  const manejarClic = () => {
+    setNumClic(numClics + 1);
+  }
+
+  const manejarClicReiniciar = () => {
+    setNumClic(0);
+  }
+
   return (
     <div className="App">
-      <div className='freecodecamp-logo-contenedor'>
-      <img className='freecodecamp-logo' src={freeCodeCampLogo} alt='Logo de freeCodeCamp'/>
+      <div className='contador-title'>
+        <h1>Contador</h1>
       </div>
       <div className='contenedor-principal'>
-        
+        <Contador numClicks={numClics} />
+        <Boton texto='Click me' esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton texto='Reiniciar' esBotonDeClic={false} manejarClic={manejarClicReiniciar} />
+
       </div>
       
     </div>
---
+++ src/components/contador.jsx
@@ -0,0 +1,12 @@
+import '../hojas-de-estilos/contador.css'
+
+function Contador({ numClicks }){
+
+    return(
+        <div className="contador">
+            {numClicks}
+        </div>
+    );
+}
+
+export default Contador;
